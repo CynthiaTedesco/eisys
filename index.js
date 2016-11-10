eec84fe293ac118abe03ee01c8cf0b2898be8f6f
Cynthia Daniela Tedesco <cynthia.tedesco@gmail.com>
2016-11-10 00:01:21 -0300

    populating with real stuff

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
+// change the rendering engine 
 app.set('view engine', 'ejs');
 
+// passing to express the template index.js to solve first page.
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/conocenos', function(request, response) {
+  response.render('pages/conocenos', {title:'CONOCENOS'});
+});
+app.get('/contacto', function(request, response) {
+  response.render('pages/contacto', {title:'CONTACTO'});
+});
+app.get('/servicios', function(request, response) {
+  response.render('pages/servicios', {title:'SERVICIOS'});
 });
 
 app.listen(app.get('port'), function() {
